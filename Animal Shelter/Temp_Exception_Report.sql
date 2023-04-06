ee23c1cba5a85633f6504ce9372c43cc545ac09f
Aria Turner <51181122+ariarturner@users.noreply.github.com>
2023-04-06 12:23:39 -0700

    Update Temp_Exception_Report.sql

--- Animal Shelter/Temp_Exception_Report.sql
+++ Animal Shelter/Temp_Exception_Report.sql
@@ -16,61 +16,49 @@ https://dbfiddle.uk/yfNVWkwE?hide=16
 
 -- for each checkup, compare temp to avg species temp
 WITH temp_diffs AS (
-SELECT species,
-	name,
-	temperature,
-	checkup_time,
+SELECT species, name, temperature, checkup_time,
 	CAST(AVG(temperature) 
-			OVER (PARTITION BY species) 
+		OVER (PARTITION BY species) 
 		AS DECIMAL (5,2)
-		) AS species_avg_temp,
+	) AS species_avg_temp,
 	CAST(temperature - AVG(temperature) 
-					OVER (PARTITION BY species)
+		OVER (PARTITION BY species)
 		AS DECIMAL (5, 2) 
-		) AS diff_from_avg
+	) AS diff_from_avg
 FROM routine_checkups
 ),
 -- create an indicator for temperature excpetions
 exception_indicator AS (
 SELECT	*,
 	CASE 
-		WHEN ABS(diff_from_avg / species_avg_temp) >= 0.005
-		THEN 1
+		WHEN ABS(diff_from_avg / species_avg_temp) >= 0.005 THEN 1
 		ELSE 0
 	END AS is_temp_exception
 FROM temp_diffs
 ),
 -- number of exceptions per animal
 animal_exceptions AS (
-SELECT species,
-	name,
+SELECT species, name,
 	SUM (is_temp_exception) AS num_exceptions,
-	MAX (CASE 
-			WHEN is_temp_exception = 1 
-			THEN checkup_time
+	MAX(CASE 
+			WHEN is_temp_exception = 1 THEN checkup_time
 			ELSE NULL
-			END
-		) AS latest_exception
+		END
+	) AS latest_exception
 FROM exception_indicator
-GROUP BY species, name
+	GROUP BY species, name
 ),
--- place animals into ntiles by number of exceptions for each species
+-- place animals into ntiles by number of exceptions for each species; ntiles used because most closely divides species by 4
 exceptions_ntile AS (
 SELECT 	*,
 	NTILE (4)
-	OVER (PARTITION BY species 
-			ORDER BY num_exceptions ASC,
-				 latest_exception DESC
+		OVER (PARTITION BY species 
+			ORDER BY num_exceptions ASC, latest_exception DESC
 		) AS ntile
 FROM animal_exceptions
 )
 -- only look at the top ntile (top 25%)
-SELECT species,
-	name,
-	num_exceptions,
-	latest_exception
+SELECT species, name, num_exceptions, latest_exception
 FROM exceptions_ntile
-WHERE ntile = 1
-ORDER BY species ASC,
-		num_exceptions DESC,
-		latest_exception DESC;
+	WHERE ntile = 1
+	ORDER BY species ASC, num_exceptions DESC, latest_exception DESC;
